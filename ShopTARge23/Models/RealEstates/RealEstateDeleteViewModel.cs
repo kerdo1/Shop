fcc2771fc074107360d21980b09f6b874874ad11
kerdo1 <kerdovahk@gmail.com>
2024-12-02 08:58:06 +0200

    Create RealEstateDeleteViewModel.cs

---
+++ ShopTARge23/Models/RealEstates/RealEstateDeleteViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ShopTARge23.Models.RealEstates
+{
+    public class RealEstateDeleteViewModel
+    {
+        public Guid? Id { get; set; }
+        public string? Location { get; set; }
+        public double? Size { get; set; }
+        public int? RoomNumber { get; set; }
+        public string? BuildingType { get; set; }
+
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? ModifiedAt { get; set; }
+
+        public List<RealEstateImageViewModel> Image { get; set; }
+            = new List<RealEstateImageViewModel>();
+    }
+}
