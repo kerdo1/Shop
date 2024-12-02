3b602b8ffacc610946b047adca668ca5587e29ec
kerdo1 <kerdovahk@gmail.com>
2024-12-02 09:03:58 +0200

    Update FileToDatabaseDto.cs

--- ShopTARge23.Core/Dto/FileToDatabaseDto.cs
+++ ShopTARge23.Core/Dto/FileToDatabaseDto.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ShopTARge23.Core.Dto
 {
-    internal class FileToDatabaseDto
+    public class FileToDatabaseDto
     {
+        public Guid Id { get; set; }
+        public string? ImageTitle { get; set; }
+        public byte[]? ImageData { get; set; }
+        public Guid? RealEstateId { get; set; }
     }
 }
