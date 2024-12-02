ec9da9a233b0926e28d5cdfdb3be6d80cc5eaa1a
kerdo1 <kerdovahk@gmail.com>
2024-12-02 09:13:51 +0200

    Update RealEstateDto.cs

--- ShopTARge23.Core/Dto/RealEstateDto.cs
+++ ShopTARge23.Core/Dto/RealEstateDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,5 +16,8 @@ namespace ShopTARge23.Core.Dto
         public string BuildingType { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ModifiedAt { get; set; }
+        public List<IFormFile> Files { get; set; }
+        public IEnumerable<FileToDatabaseDto> Image { get; set; }
+            = new List<FileToDatabaseDto>();
     }
 }
